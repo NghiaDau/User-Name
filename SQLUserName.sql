a2ef33c528b1938aafe8e4b26f7f99e09bf07f14
VanNghia <nghiadauvan@gmail.com>
2023-09-19 12:19:24 +0700

    them sql

---
+++ SQLUserName.sql
@@ -0,0 +1,15 @@
+create database UserNameDB
+go
+
+use UserNameDB
+go
+
+create table DataUser
+(
+	UserName nvarchar(30) not null,
+	FullName nvarchar (60) not null,
+	Email nvarchar(50) not null,
+	Pasword nvarchar(20) not null,
+	Dateofbirth Date,
+	constraint DataUser_PK primary key(Username)
+)
